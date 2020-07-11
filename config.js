c7ec423a4d365922fc844ac54671e5f09eec84e6
Zippoooo <36099425+Zippoooo@users.noreply.github.com>
2020-07-11 21:33:06 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+var config = {};
+
+config.color = "" // Only Hex color / Example : #FFFF00 (Thats yellow...) / More colors in this site https://htmlcolorcodes.com
+
+config.token = "" // Bot token example in the docs.
+
+config.id = "" // Message id. More information in the docs.
+
+config.channelid = "" // Channel ID. More information in the docs
+
+config.servers = 
+    [{
+        name: "Server Name", // Something like "Surf Combat" "Retake"
+        ip: "IDK your server ip", // Server IP. No Port. No Domain. Only numbers!!!
+        port: Your-server-port // Server port. after the ip theres : this is the port. (You should know that...)
+        }]
+
+
+module.exports = config
